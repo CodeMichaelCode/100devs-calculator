56ec428111c8dc9491af60b783c67b9824ece584
Michael <mrolawunmi@gmail.com>
2022-04-05 01:48:21 -0400

    Added Clear Button

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let screen = document.querySelector('#screen');
 const total = document.querySelector('#valueEqual');
 const button = document.querySelectorAll('.button');
 const calcBody = document.querySelector('section');
+const clearBtn = document.querySelector('#clear');
 
 //Objects
 function Ti86 () {
@@ -15,7 +16,9 @@ function Ti86 () {
         console.log(clickElement);
         screen.innerText = (screen.innerText += clickElement);
     }
-
+    this.clear = function() {
+        screen.innerText = ''
+    }
 }
 
 let calc = new Ti86;
@@ -23,3 +26,4 @@ let calc = new Ti86;
 //Event Listeners
 let buttonTarget = calcBody;
 buttonTarget.addEventListener('click', calc.addValue);
+clearBtn.addEventListener('click', calc.clear);
